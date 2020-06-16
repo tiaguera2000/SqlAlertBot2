313a19358be41c0f4c362a560ad4bfe73a69a7e9
Tiaguera <tiago_mohaa@hotmail.com>
2020-06-16 18:57:09 -0300

    Adicionar arquivos de projeto.

---
+++ SqlAlertBot2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace SqlAlertBot2
+{
+    class Program
+    {
+        private static readonly TelegramBotClient bot = new TelegramBotClient("1234289330:AAHE5ZCIfVXXhaS2dwzNiFcwnaWyH6FgJys");
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            /*bot.OnMessage += Bot_OnMessage;
+            bot.StartReceiving();
+            Console.ReadLine();
+            bot.StopReceiving();*/
+
+            await bot.SendTextMessageAsync("-444824024", "olá!");
+        }
+        /*private static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+        }*/
+    }
+}
